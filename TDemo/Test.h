24e9ad8aea15d75ae3642f4f5a6a6cbfbab078dc
intersilindia <apple@apples-iMac.local>
2014-05-23 14:24:49 +0530

    Merge branch 'Test'

---
+++ TDemo/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  TDemo
+//
+//  Created by apple on 5/23/14.
+//  Copyright (c) 2014 Intersil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ TDemo/Test.m
@@ -0,0 +1,17 @@
+//
+//  Test.m
+//  TDemo
+//
+//  Created by apple on 5/23/14.
+//  Copyright (c) 2014 Intersil. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+-(void)awakeFromNib
+{
+    NSLog(@"changes done here");
+    
+}
+@end
